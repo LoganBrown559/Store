215745b1c03cd756a51905ae997626495d79e7c4
Logan Brown <logmbrow@ut.utm.edu>
2021-01-27 18:31:07 -0800

    Add project files.

---
+++ Store/AbItem.cs
@@ -0,0 +1,328 @@
+﻿/*
+ *
+ * Name: Logan Brown
+ * File: AbItem.cs
+ * Date: 1/27/2020
+ * Description: Holds the abstract item class, as well as three child classes.
+ *
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store
+{
+    public abstract class AbItem
+    {
+        protected string type;
+        protected string name;
+        protected int quantity;
+        protected double price;
+
+        //A basic constructer, no class inherits this.
+        public AbItem()
+        {
+            type = "None";
+            name = "None";
+            quantity = 0;
+            price = 0.00;
+        }
+
+        //I will define all the methods here, as all of them are overriden in the same way, if not very similar
+
+        /*
+         * Name: checkPrice
+         * 
+         * Parameters: None
+         * 
+         * Description: Returns Price, I made a different function with a different name
+         */
+        public abstract double checkPrice();
+        /*
+         * Name: printInfo
+         * 
+         * Parameters: None
+         * 
+         * Description: Prints the info of the item; All variables of the class are printed with a label
+         */
+        public abstract void printInfo();
+        /*
+         * Name: purchase
+         * 
+         * Parameters: None
+         * 
+         * Description: Reduces quantity by specified amount. Again, replaced with a function I like better.
+         */
+        public abstract void purchase();
+        /*
+         * Name: getName
+         * 
+         * Parameters: None
+         * 
+         * Description: returns the name variable
+         */
+        public abstract string getName();
+        /*
+         * Name: getPrice();
+         * 
+         * Parameters: None
+         * 
+         * Description: Returns price variable
+         */
+        public abstract double getPrice();
+        /*
+         * Name: setQuantity
+         * 
+         * Parameters:
+         *      int amount - amount to change Quantity variable. Make negative to subtract from quantity
+         *      
+         * Description: Changes quantity to simulate a purchase or restock
+         */
+        public abstract void setQuantity(int amount);
+    }
+
+    /*
+     * Here starts the first child class
+     *
+     */
+
+    public class Vegetable : AbItem
+    {
+        private string color;
+        
+        public Vegetable()
+        {
+            type = "None";
+            name = "None";
+            quantity = 0;
+            price = 0.00;
+            color = "None";
+        }
+
+        public Vegetable(string typeVar, string nameVar, int quantityVar, double priceVar, string colorVar)
+        {
+            type = typeVar;
+            name = nameVar;
+            quantity = quantityVar;
+            price = priceVar;
+            color = colorVar;
+        }
+        public override double checkPrice()
+        {
+            return price;
+        }
+
+        public override void printInfo()
+        {
+            Console.WriteLine("Type: " + type);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Quantity: " + quantity);
+            Console.WriteLine("Price: " + price);
+            Console.WriteLine("Color: " + color);
+        }
+
+        public override void purchase()
+        {
+            int numItem;
+            Console.Write("How many item are being purchased? ");
+
+            try
+            {
+                numItem = Convert.ToInt32(Console.ReadLine());
+                while (numItem < 0)
+                {
+                    Console.WriteLine("Invalid Number.");
+                    Console.Write("How many item are being purchased? ");
+                    numItem = Convert.ToInt32(Console.ReadLine());
+                }
+
+                quantity -= numItem;
+            }
+            catch
+            {
+                Console.WriteLine("Whoops! There has been an error. Hit enter to continue.");
+                Console.ReadLine();
+            }
+        }
+
+        public override string getName()
+        {
+            return name;
+        }
+
+        public override double getPrice()
+        {
+            return price;
+        }
+
+        public override void setQuantity(int amount)
+        {
+            // amount should be negative to lessen amount
+            quantity += amount;
+        }
+    }
+
+    /*
+     * Next child class starts here!
+     * 
+     */
+
+    public class Textbook : AbItem
+    {
+        private int pageNum;
+
+        public Textbook()
+        {
+            type = "None";
+            name = "None";
+            quantity = 0;
+            price = 0.00;
+            pageNum = 0;
+        }
+
+        public Textbook(string typeVar, string nameVar, int quantityVar, double priceVar, int pageNumVar)
+        {
+            type = typeVar;
+            name = nameVar;
+            quantity = quantityVar;
+            price = priceVar;
+            pageNum = pageNumVar;
+        }
+        
+        public override double checkPrice()
+        {
+            return price;
+        }
+
+        public override void printInfo()
+        {
+            Console.WriteLine("Type: " + type);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Quantity: " + quantity);
+            Console.WriteLine("Price: " + price);
+            Console.WriteLine("Pages: " + pageNum);
+        }
+
+        public override void purchase()
+        {
+            int numItem;
+            Console.Write("How many item are being purchased? ");
+
+            try
+            {
+                numItem = Convert.ToInt32(Console.ReadLine());
+                while (numItem < 0)
+                {
+                    Console.WriteLine("Invalid Number.");
+                    Console.Write("How many item are being purchased? ");
+                    numItem = Convert.ToInt32(Console.ReadLine());
+                }
+
+                quantity -= numItem;
+            }
+            catch
+            {
+                Console.WriteLine("Whoops! There has been an error. Hit enter to continue.");
+                Console.ReadLine();
+            }
+        }
+
+        public override string getName()
+        {
+            return name;
+        }
+
+        public override double getPrice()
+        {
+            return price;
+        }
+
+        public override void setQuantity(int amount)
+        {
+            // amount should be negative to lessen amount
+            quantity += amount;
+        }
+    }
+
+    /*
+     * Next child class here!
+     *
+     */
+
+    public class VideoGame : AbItem
+    {
+        private string genre;
+
+        public VideoGame()
+        {
+            type = "None";
+            name = "None";
+            quantity = 0;
+            price = 0.00;
+            genre = "None";
+        }
+
+        public VideoGame(string typeVar, string nameVar, int quantityVar, double priceVar, string genreVar)
+        {
+            type = typeVar;
+            name = nameVar;
+            quantity = quantityVar;
+            price = priceVar;
+            genre = genreVar;
+        }
+
+        public override double checkPrice()
+        {
+            return price;
+        }
+
+        public override void printInfo()
+        {
+            Console.WriteLine("Type: " + type);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Quantity: " + quantity);
+            Console.WriteLine("Price: " + price);
+            Console.WriteLine("Genre: " + genre);
+        }
+
+        public override void purchase()
+        {
+            int numItem;
+            Console.Write("How many item are being purchased? ");
+
+            try
+            {
+                numItem = Convert.ToInt32(Console.ReadLine());
+                while (numItem < 0)
+                {
+                    Console.WriteLine("Invalid Number.");
+                    Console.Write("How many item are being purchased? ");
+                    numItem = Convert.ToInt32(Console.ReadLine());
+                }
+
+                quantity -= numItem;
+            }
+            catch
+            {
+                Console.WriteLine("Whoops! There has been an error. Hit enter to continue.");
+                Console.ReadLine();
+            }
+        }
+
+        public override string getName()
+        {
+            return name;
+        }
+
+        public override double getPrice()
+        {
+            return price;
+        }
+
+        public override void setQuantity(int amount)
+        {
+            // amount should be negative to lessen amount
+            quantity += amount;
+        }
+    }
+}
---
+++ Store/Program.cs
@@ -0,0 +1,262 @@
+﻿/*
+ *
+ * Name: Logan Brown
+ * File: Program.cs
+ * Date: 1/27/2020
+ * Description: Holds main driver for program, as well as a Item Manipulation Class to assist the driver in its functions.
+ *
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Store
+{
+    class Program
+    {
+        //Here is the main driver
+        static void Main(string[] args)
+        {
+            //The main list that holds all items
+            List<AbItem> store = new List<AbItem>();
+
+            //Simple Menu
+            Console.WriteLine();
+            Console.WriteLine("Welcome to Malacai's Magical Mystical Menagerie of Mutated Multi-Purpose Wares!");
+            Console.WriteLine("Choose an option by typing a number:");
+            Console.WriteLine();
+            Console.WriteLine("1. Add Item");
+            Console.WriteLine("2. Check a Price");
+            Console.WriteLine("3. Check Information");
+            Console.WriteLine("4. Purchase Item");
+            Console.WriteLine("5. Exit Application");
+            Console.WriteLine();
+            Console.Write("Enter your choice: ");
+
+            //Main number that is a choice from the main menu
+            int choice = Convert.ToInt32(Console.ReadLine());
+            //Used for a sub menu in choice 1
+            int option;
+
+            while (choice != 5)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        //I do error handling here because it's EASY for someone to try and type the word
+                        try
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("What is the item type?");
+                            Console.WriteLine("1. Vegetable");
+                            Console.WriteLine("2. Textbook");
+                            Console.WriteLine("3. Video Game");
+                            Console.Write("Enter a correspondeing number: ");
+                            option = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine();
+                        }
+                        catch
+                        {
+                            //This ensures the loop goes again
+                            option = 0;
+                        }
+
+                        //Calls one of the create methods of ItemManip
+                        if (option == 1)
+                        {
+                            store.Add(ItemManip.createVegi());
+                        }
+                        else if (option == 2)
+                        {
+                            store.Add(ItemManip.createTextbook());
+                        }
+                        else if (option == 3)
+                        {
+                            store.Add(ItemManip.createGame());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Number.");
+                        }
+                        Console.WriteLine("Item Added!");
+                        break;
+
+                    case 2:
+                        //A simple find name and return price
+                        Console.Write("Name of Item: ");
+                        string name = Console.ReadLine();
+                        
+                        for(int i = 0; i < store.Count; i++)
+                        {
+                            if (name == store[i].getName())
+                            {
+                                Console.WriteLine("The price is: " + store[i].getPrice());
+                            }
+                        }
+                        
+                        break;
+
+                    case 3:
+                        //prints a list then the user chooses a number corresponding
+                        Console.WriteLine();
+                        Console.WriteLine("Choose the corresponding number to the item info you want: ");
+                        ItemManip.printList(store);
+
+                        Console.Write("Which number do you want? ");
+                        try
+                        {
+                            int selection = Convert.ToInt32(Console.ReadLine());
+
+                            //This is -1 because the user sees a list starting at 1
+                            store[selection - 1].printInfo();
+                        }
+                        catch
+                        {
+                            //I've triggered this on accident before. Glad I made a catch!
+                            Console.WriteLine("I don't know what you did, but you nearly crashed the program.");
+                        }
+                        break;
+
+                    case 4:
+                        Console.WriteLine();
+                        Console.WriteLine("Choose item to purchase : ");
+                        ItemManip.printList(store);
+
+                        Console.Write("Which number do you want? ");
+                        try
+                        {
+                            int selection = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("And how many are we buying? ");
+                            int purchaseNum = Convert.ToInt32(Console.ReadLine());
+                            
+                            //We make it negative for the setter function
+                            purchaseNum = purchaseNum - (purchaseNum * 2);
+
+                            //This is -1 because the user sees a list starting at 1
+                            store[selection - 1].setQuantity(purchaseNum);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("I don't know what you did, but you nearly crashed the program.");
+                        }
+                        break;
+
+                    case 5:
+                        Console.WriteLine();
+                        Console.WriteLine("Closing Program...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid number entered!");
+                        Console.WriteLine();
+                        break;
+                }
+
+                //This prevents the menu from being printed again if the user wants to quit
+                if (choice != 5)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Welcome to Malacai's Magical Mystical Menagerie of Mutated Multi-Purpose Wares!");
+                    Console.WriteLine("Choose an option by typing a number:");
+                    Console.WriteLine();
+                    Console.WriteLine("1. Add Item");
+                    Console.WriteLine("2. Check a Price");
+                    Console.WriteLine("3. Check Information");
+                    Console.WriteLine("4. Purchase Item");
+                    Console.WriteLine("5. Exit Application");
+                    Console.WriteLine();
+                    Console.Write("Enter your choice: ");
+
+                    choice = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Farewell!");
+        }
+    }
+
+    /*
+     * 
+     * The following is a class is for item manipulation and easily resuable code. It's just handy!
+     * 
+     */
+    
+    public class ItemManip
+    {
+        
+        //Creates a Vegi object when needed! Asks all questions needed
+        public static Vegetable createVegi()
+        {
+            Console.Write("What is the Vegetable name? ");
+            string name = Console.ReadLine();
+            
+            Console.Write("How many are we adding? ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("What is the retail price? ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("What is the color of the Vegetable? ");
+            string color = Console.ReadLine();
+
+            Vegetable rValue = new Vegetable("Vegetable", name, quantity, price, color);
+
+            return rValue;
+
+        }
+        
+        //Make a textbook hot off the press when needed. Asks all data needed.
+        public static Textbook createTextbook()
+        {
+            Console.Write("What is the Textbook name? ");
+            string name = Console.ReadLine();
+
+            Console.Write("How many are we adding? ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What is the retail price? ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("How many pages are in the book? ");
+            int pages = Convert.ToInt32(Console.ReadLine());
+
+            Textbook rValue = new Textbook("Textbook", name, quantity, price, pages);
+
+            return rValue;
+
+        }
+
+        //Creates a Video Game object. Collects all data needed
+        public static VideoGame createGame()
+        {
+            Console.Write("What is the Video Game name? ");
+            string name = Console.ReadLine();
+
+            Console.Write("How many are we adding? ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What is the retail price? ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("What is the Genre? ");
+            string genre = Console.ReadLine();
+
+            VideoGame rValue = new VideoGame("Video Game", name, quantity, price, genre);
+
+            return rValue;
+
+        }
+
+        //Takes any list given (that contains items) and prints each item name to console
+        public static void printList(List<AbItem> itemList)
+        {
+
+            Console.WriteLine("Here is the list of items:");
+            Console.WriteLine();
+
+            for (int i = 0; i < itemList.Count; i++)
+            {
+                Console.WriteLine((i+1) + ". " + itemList[i].getName());
+                
+            }
+        }
+    }
+}
